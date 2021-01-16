db4da7e6e5a56f8bf305d1e5e9935f3b856d3de8
@david.connell <david@lumient.com.au>
2021-01-16 15:02:09 +1030

    add heroku commands

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+app.use(express.static("client/build"));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
