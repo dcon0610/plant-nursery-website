359d7ff080614e635130ddfa47ca276a24baf41b
@david.connell <david@lumient.com.au>
2021-01-18 23:07:27 +1030

    testing OR function mongodb

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 
 const express = require("express");
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 const mongoose = require("mongoose");
-const routes = require("./routes");
+//const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 let resultsArray=[]
@@ -17,50 +17,31 @@ app.use(express.json());
 ;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('/api/plants',(request,response) => {
-    MongoClient.connect(process.env.MONGODB_URI, function(err, database){
-    console.log("connected with this!")
-    const database1 = database.db('plantwebsite')
-    const json = database1.collection('plants').find({}).toArray(function(err, str){
-    
-      if (err) throw err;
-    
-    response.json({plants: str})
-        database.close();
-    })
-    
-    
-    
-    
-    
-    })
-    })
 }
 else {
-  app.get('/api/plants',(request,response) => {
-    MongoClient.connect("mongodb://localhost/plantwebsite", function(err, database){
-    console.log("connected with this!")
-    const database1 = database.db('plantwebsite')
-    const json = database1.collection('plants').find({}).toArray(function(err, str){
-    
-      if (err) throw err;
-    
-    response.json({plants: str})
-        database.close();
-    })
-    
-    
-    
-    
-    
-    })
-    })
+ 
 }
 // Add routes, both API and view
 
+app.get('/api/plants',(request,response) => {
+  MongoClient.connect(process.env.MONGODB_URI ||"mongodb://localhost/plantwebsite", function(err, database){
+  console.log("connected with this!")
+  const database1 = database.db('plantwebsite')
+  const json = database1.collection('plants').find({}).toArray(function(err, str){
+  
+    if (err) throw err;
+  
+  response.json({plants: str})
+      database.close();
+  })
+  
+  
+  
+  
+  
+  })
+  })
 
-// Connect to the Mongo DB
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/plantwebsite");
 
 
 // Start the API server
